ac52638c7f1e7c94d3d506c5acb29ddc382bebae
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-24 16:21:39 +0530

    Add files via upload

---
+++ Hybrid/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Hybrid_inheritance
+{
+	class GrandFather
+	{
+
+		public void land()
+		{
+			Console.WriteLine("GradFater's land");
+		}
+
+	}
+
+	class Father : GrandFather
+	{
+
+		public void home()
+		{
+			Console.WriteLine("Father's home");
+		}
+
+		public void Car()
+		{
+			Console.WriteLine("Father's Car");
+		}
+	}
+
+	
+	class Son : Father
+	{
+
+		
+		public Son()
+		{
+			Console.WriteLine("Son...");
+		}
+
+		public void mobile()
+		{
+			Console.WriteLine("Son's mobile");
+		}
+	}
+
+	class Daughter : Father
+	{
+
+		
+		public Daughter()
+		{
+			Console.WriteLine("Daughter...");
+		}
+
+		public void purse()
+		{
+			Console.WriteLine("Daughter's purse");
+		}
+	}
+
+	
+	public class TestHybridInheritance
+	{
+
+		public static void Main(String[] args)
+		{
+
+			
+			Son s = new Son();
+			s.land();
+			s.Car(); 
+			s.home();
+			s.mobile();
+
+			
+			Daughter d = new Daughter();
+			d.land();
+			d.Car(); 
+			d.home();
+			d.purse();
+
+		}
+
+	}
+
+}
+
