2623ea4003b403e120a5ad2d67f4cb8818a91f96
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-24 16:06:06 +0530

    Add files via upload

---
+++ Single/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Single_Level_Inheritance
+{
+    class Father
+    {
+
+        public void home()
+        {
+            Console.WriteLine("Father's home");
+        }
+
+        public void Car()
+        {
+            Console.WriteLine("Father's Car");
+        }
+    }
+
+    //Inherit /derived / extends
+    class Son : Father
+    {
+
+        public void mobile()
+        {
+            Console.WriteLine("Son's mobile");
+        }
+    }
+
+
+
+
+    public class CSharpinheritance
+    {
+
+        public static void Main(String[] args)
+        {
+
+            Son s = new Son();
+            s.mobile();
+            s.Car();
+            s.home();
+
+        }
+
+    }
+}
+
