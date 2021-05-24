21977fe6058c184e82cc280124e2c70f205c1342
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-24 16:18:28 +0530

    Add files via upload

---
+++ Hierarchical/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Hierarchical_inheritance
+{
+	class Teacher
+	{
+
+		public void programming()
+		{
+			Console.WriteLine("Java programming...");
+		}
+
+		public void physics()
+		{
+
+			Console.WriteLine("Physics...");
+		}
+
+		public void chemistry()
+		{
+
+			Console.WriteLine("Chemistry...");
+
+		}
+	}
+
+	
+	class ComputerDepartment : Teacher
+	{
+
+		public void learn()
+		{
+			Console.WriteLine("ComputerDepartment : Learn...");
+		}
+
+	}
+
+	
+	class ScienceDepartment : Teacher
+	{
+
+		public void learn()
+		{
+			Console.WriteLine("\nScienceDepartment : Learn...");
+		}
+	}
+
+	
+	public class TestHierarchicalInheritance
+	{
+
+		public static void Main(String[] args)
+		{
+
+			ComputerDepartment cd = new ComputerDepartment();
+			cd.learn();
+			cd.programming();
+
+			
+			ScienceDepartment sd = new ScienceDepartment();
+			sd.learn();
+			sd.physics();
+			sd.chemistry();
+
+		}
+
+	}
+}
+
